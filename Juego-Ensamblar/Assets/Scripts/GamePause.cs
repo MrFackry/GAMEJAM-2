74a44e61f6dc1f48bb1b0291093d6eddcdd279b2
Carlos Andres Pallares <andrespallaresvallejo@gmail.com>
2025-05-19 16:26:43 -0500

    2
    Subida 2

--- Juego-Ensamblar/Assets/Scripts/GamePause.cs
+++ Juego-Ensamblar/Assets/Scripts/GamePause.cs
@@ -1,9 +1,30 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GamePause : MonoBehaviour
 {
+    [SerializeField] private GameObject bottonPause;
+    [SerializeField] private GameObject menuPause;
+    
     public void Pause()
     {
         Time.timeScale = 0f;
+        bottonPause.SetActive(false);
+        menuPause.SetActive(true);
     }
+
+    public void Continue()
+    {
+        Time.timeScale = 1f;
+        bottonPause.SetActive(true);
+        menuPause.SetActive(false);
+    }
+    public void restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        bottonPause.SetActive(true);
+        menuPause.SetActive(false);
+    }
+
 }
